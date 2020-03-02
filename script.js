269a4c3c4855cc2bdf6624fb29e62a16f11fc264
Timofey Chernyavskiy <t.chernyavskiy@bars.group>
2020-03-02 13:34:35 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,95 @@
+const fs = require('fs');
+const xpath = require('xpath');
+const parser = require('xmldom').DOMParser;
+const xmlserializer = require('xmlserializer');
+const yargs = require('yargs');
+const path = require('path');
+
+const options = yargs
+  .usage("Usage: node script.js -f <front/path> -b <back/path> -n <form_name>")
+  .option("f", {alias: "front",
+    describe: "Path to frontend app, include root dir <vimis-app-onko>",
+    type: "string",
+    demandOption: true
+  })
+  .option("b", {
+    alias: "back",
+    describe: "Path to backend app, include root dir <vimis-ps01-back-js>",
+    type: "string",
+    demandOption: true
+  })
+.option('n', {
+  alias: "name",
+  describe: "Name of form to edit without extension",
+  type: "string",
+  demandOption: true
+})
+.argv;
+
+(async () => {
+  const fdir = path.join(options.front, 'modules');
+  const bdir = path.join(options.back, 'modules', 'storage', 'forms');
+
+  const forms = [];
+  const findFrm = async (path2file) => {
+    let files = await fs.promises.readdir(path2file);
+    files = files.filter( f => !f.startsWith('.'));
+    for (let file of files) {
+      let fpath = path.join(path2file, file);
+      const stats = await fs.promises.stat(fpath);
+      if (stats.isDirectory()) {
+        await findFrm(fpath);
+      } else if (stats.isFile()) {
+        let { ext, name } = path.parse(fpath);
+        //console.log(`${name}${ext}`);
+        if ( ext == '.frm' && name == options.name) {
+          console.log(ext, name);
+          forms.push(fpath);
+        }
+      }
+    }
+  };
+
+  await findFrm(fdir);
+  console.log(forms.length);
+  forms.forEach( async frm => {
+    let frmName = frm.split('/').pop().split('.').shift();
+    console.log(frmName);
+    const form = await fs.promises.readFile(frm, 'utf8');
+    /*
+    const frontdoc = new parser().parseFromString(form, 'text/xml');
+    let datasets = xpath.select('//nf-dataset', frontdoc);
+    let actions = xpath.select('//nf-action', frontdoc);
+    datasets.forEach( ds => {
+      ds.setAttribute('provider', 'ehr');
+      ds.setAttribute('endpoint', `/dataset/${frmName}/${ds.getAttribute('id')}`);
+    });
+
+    actions.forEach( a => a.setAttribute('provider', 'ehr'));
+    try {
+      await fs.promises.writeFile(frm, xmlserializer.serializeToString(frontdoc), 'utf-8');
+    } catch(err) {
+      console.log(err);
+    }
+
+*/
+    const backdoc = new parser().parseFromString(form, 'text/xml');
+    const nodes = xpath.select('//*', backdoc);
+    for (let node of nodes) {
+      let name = node.localName;
+      if (name !== 'nf-form' && name !== 'nf-dataset' && name !== 'nf-action' && name !== 'nf-action-on') {
+        node.parentNode.removeChild(node);
+      }
+    }
+
+    //const bpath = `${frm.split(fdir).shift()}/${bdir}${frmName}_back.frm`;
+    const bpath = path.join(bdir, `${frmName}_back.frm`);
+    console.log(bpath);
+    try {
+      await fs.promises.writeFile(bpath, xmlserializer.serializeToString(backdoc), 'utf8');
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+})();
